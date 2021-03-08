22e7b2f8a4f9cf3ca87d0494ef0ad8f7e43237d4
hawker9 <80215939+hawker9@users.noreply.github.com>
2021-03-07 19:43:39 -0800

    commit on 'main'

--- hello/views.py
+++ hello/views.py
@@ -9,7 +9,7 @@ import requests
 def index(request):
     r = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=SPXL&outputsize=full&apikey=8DI2YTBO8A4SZ5BR&datatype=csv')
     print(r.text)
-    return HttpResponse('<pre>' "Wills website!" + r.text +  '</pre>')
+    return HttpResponse('<pre>' "Wills website! Below is an API to the stcok data for SPXL ticker:" + r.text +  '</pre>')
 
 
 def db(request):
