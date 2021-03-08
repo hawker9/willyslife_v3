1cb6bf85424734966a28b187e0e32e67ac064046
hawker9 <80215939+hawker9@users.noreply.github.com>
2021-03-07 18:06:09 -0800

    commit on "main'

--- hello/views.py
+++ hello/views.py
@@ -9,7 +9,7 @@ import requests
 def index(request):
     r = requests.get('http://httpbin.org/status/418')
     print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    return HttpResponse('<pre>' + r.text + "Wills website!" + '</pre>')
 
 
 def db(request):
