e4620bb58f156853fb159366c34757728a374636
hawker9 <80215939+hawker9@users.noreply.github.com>
2021-03-09 19:59:10 -0800

    changeing pages around

--- hello/views.py
+++ hello/views.py
@@ -8,13 +8,14 @@ import requests
 
 # Create your views here.
 def index(request):
+    return HttpResponse('<pre><b><p style=font-size:48px>&#128293&#128293Wills website!&#128293&#128293</b></p><br><br><a href=https://willyslife.herokuapp.com/db/>SPXL Stock Information</a></pre>')
+
+   
+def db(request):
     r = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=SPXL&outputsize=full&apikey=8DI2YTBO8A4SZ5BR&datatype=json')
     print(r.text)
-    return HttpResponse('<pre><b>'"<a href=https://willyslife.herokuapp.com/db/>Other Page</a><p style=font-size:48px>&#128151Wills website!&#128151</b></p><br><br>Below is an API to the stock data for SPXL ticker:" +'<br><br><br>'+ r.text +  '</pre>')
+    return HttpResponse('<pre><b>'"<p style=font-size:48px>&#128151&#128151&#128151&#128151</b></p><br><br>Below is an API to the stock data for SPXL ticker:" +'<br><br><br>'+ r.text +  '</pre>')
 
-
-def db(request):
-    return HttpResponse('<pre><b><p style=font-size:48px>&#128293&#128293&#128293&#128293</b></p><br><br>Page # 2 of Willy</pre>')
 
 
 
