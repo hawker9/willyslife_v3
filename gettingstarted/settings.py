95e4bf7182bbd41854668d77c0ea7a7994fb9aa5
hawker9 <80215939+hawker9@users.noreply.github.com>
2021-03-09 19:06:19 -0800

    commit on 'main'

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "Admin.urls"
+ROOT_URLCONF = "gettingstarted.urls"
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     }
 ]
 
-WSGI_APPLICATION = "Admin.wsgi.application"
+WSGI_APPLICATION = "gettingstarted.wsgi.application"
 
 
 # Database
