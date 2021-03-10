94429ebbe79921ef44bfd09591e8225173ee0da6
hawker9 <80215939+hawker9@users.noreply.github.com>
2021-03-09 19:21:31 -0800

    commit on 'main'

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import django_heroku
-
+import sys
+import site
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
