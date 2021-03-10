86a0400078ba77e0629b8d88e73eb4aad1c4c644
hawker9 <80215939+hawker9@users.noreply.github.com>
2021-03-09 22:01:08 -0800

    test db 8

--- hello/views.py
+++ hello/views.py
@@ -38,7 +38,7 @@ def databaseadd(request):
     cursor = conn.cursor()
 
     #Creating table as per requirement
-    sql2 = '''INSERT INTO Votes (id, name) VALUES (45,'TEST');'''
+    sql2 = '''INSERT INTO Votes (name) VALUES ('TEST');'''
 
     print(sql2)
     cursor.execute(sql2)
