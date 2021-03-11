3c67e6cdf706305fca9ed3abd6e515e4f33df790
hawker9 <80215939+hawker9@users.noreply.github.com>
2021-03-11 03:52:03 -0800

    12

--- polls/views.py
+++ polls/views.py
@@ -20,7 +20,7 @@ def detail(request, question_id):
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('index.html')
+    template = loader.get_template("index.html")
     context = {
         'latest_question_list': latest_question_list,
     }
