58b2959b7790b65e10c8157a4b3a31d2f068fd9b
hawker9 <80215939+hawker9@users.noreply.github.com>
2021-03-11 02:57:11 -0800

    fixing

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -20,11 +20,5 @@ urlpatterns = [
     path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
     path("databaseadd/", hello.views.databaseadd, name="databaseadd"),
-
-    path('', polls.views.index, name='index'),
-    path('<int:question_id>/', polls.views.detail, name='detail'),
-    path('<int:question_id>/results/', polls.views.results, name='results'),
-    path('<int:question_id>/vote/', polls.views.vote, name='vote'),
-    path('<int:question_id>/', polls.views.detail, name='detail'),
 ]
-  ]
+  
