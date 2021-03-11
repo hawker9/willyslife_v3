23e53f4b4fbcf432d3f37c8c3697453b8a29a5fb
hawker9 <80215939+hawker9@users.noreply.github.com>
2021-03-10 16:59:25 -0800

    polls upload

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 import hello.views
-
+import polls.views
 
 # To add a new path, first import the app:
 # import blog
@@ -20,4 +20,11 @@ urlpatterns = [
     path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
     path("databaseadd/", hello.views.databaseadd, name="databaseadd"),
+
+    path('', polls.views.index, name='index'),
+    path('<int:question_id>/', polls.views.detail, name='detail'),
+    path('<int:question_id>/results/', polls.views.results, name='results'),
+    path('<int:question_id>/vote/', polls.views.vote, name='vote'),
+    path('<int:question_id>/', polls.views.detail, name='detail'),
+]
   ]
